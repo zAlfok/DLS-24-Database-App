74584c2155a56cb109cfa4cc3d7a422279c32dd7
zAlfok <131631017+zAlfok@users.noreply.github.com>
2024-06-28 00:16:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTlAJXDac7kQ4rgQM57RjLmbKw1KaAzL0XMIEFWanLVn7rQSGqXQy5bFxLTnxYB3Q/pub?output=csv')
+fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTdSHbV58pvz1mflTyHvgwgCB4gEQ38PHsqw30q0tLocoES5RcypT9OVLkjQdlQsUx0r_NpcRxvtB5s/pub?output=csv')
   .then(response => response.text())
   .then(data => {
-    console.log(data);
     const rows = data.split('\n');
+    
+    // Obtener los encabezados de las columnas
+    const headers = rows[0].split(',');
     
     // Omitir la primera fila que contiene los encabezados
     rows.shift();
     
+    // Obtener el elemento de la tabla
+    const table = document.getElementById('player-table');
+    
+    // Crear la fila de encabezados
+    const headerRow = document.createElement('tr');
+    headers.forEach(header => {
+      const headerCell = document.createElement('th');
+      headerCell.textContent = header;
+      headerRow.appendChild(headerCell);
+    });
+    
+    // Agregar la fila de encabezados a la tabla
+    table.querySelector('thead').appendChild(headerRow);
+    
+    // Mostrar los datos en la tabla
     rows.forEach(row => {
       const playerData = row.split(',');
       const playerRow = document.createElement('tr');
@@ -17,7 +34,7 @@ fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTlAJXDac7kQ4rgQM57RjLmbK
         playerRow.appendChild(cell);
       });
       
-      document.getElementById('player-data').appendChild(playerRow);
+      table.querySelector('tbody').appendChild(playerRow);
     });
   })
   .catch(error => console.error('Error fetching data:', error));
